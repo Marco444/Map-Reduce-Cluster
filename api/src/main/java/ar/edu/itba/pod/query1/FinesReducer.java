9d10424d9fecb13528dad3bf8e76d97d94074f98
Marco4444 <mscilipoti@itba.edu.ar>
2024-06-01 13:01:01 -0300

    added total fines by infractions sketch
    Co-authored-by: martinippo01 <martinippo@gmail.com>
    Co-authored-by: slarroude <slarroude@itba.edu.ar>
    Co-authored-by: Martín E. Zahnd <mzahnd@itba.edu.ar>

--- api/src/main/java/ar/edu/itba/pod/query1/FinesReducer.java
+++ api/src/main/java/ar/edu/itba/pod/query1/FinesReducer.java
@@ -1,4 +1,29 @@
 package ar.edu.itba.pod.query1;
 
-public class FinesReducer {
+import ar.edu.itba.pod.data.LongTuple;
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+
+public class FinesReducer implements ReducerFactory<LongTuple, Integer, Integer> {
+    @Override
+    public Reducer<Integer, Integer> newReducer(LongTuple integerPair) {
+        return new Reducer<>() {
+            private int sum;
+
+            @Override
+            public void beginReduce () {
+                sum = 0;
+            }
+
+            @Override
+            public void reduce(Integer value) {
+                sum += value;
+            }
+
+            @Override
+            public Integer finalizeReduce() {
+                return sum;
+            }
+        };
+    }
 }
--- client/src/main/java/ar/edu/itba/pod/tpe2/client/query1/TotalFinesByInfractions.java
+++ client/src/main/java/ar/edu/itba/pod/tpe2/client/query1/TotalFinesByInfractions.java
@@ -1,17 +1,36 @@
 package ar.edu.itba.pod.tpe2.client.query1;
 
 import ar.edu.itba.pod.Util;
+import ar.edu.itba.pod.data.LongTuple;
+import ar.edu.itba.pod.data.Tickets;
+import ar.edu.itba.pod.query1.FinesMapper;
+import ar.edu.itba.pod.query1.FinesReducer;
 import ar.edu.itba.pod.tpe2.client.QueryClient;
+import com.hazelcast.mapreduce.Job;
 import com.hazelcast.mapreduce.JobTracker;
 import com.hazelcast.mapreduce.KeyValueSource;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.TreeSet;
 import java.util.concurrent.ExecutionException;
 
 public class TotalFinesByInfractions extends QueryClient {
     @Override
     public void resolveQuery() throws ExecutionException, InterruptedException, IOException {
+        final JobTracker jobTracker = getHz().getJobTracker(Util.HAZELCAST_NAMESPACE);
+
+        final KeyValueSource<Integer, Tickets> source = KeyValueSource.fromMultiMap(getHz().getMultiMap(Util.HAZELCAST_NAMESPACE));
+
+        Job<Integer, Tickets> job = jobTracker.newJob(source);
+
+        Map<LongTuple, Integer> reducedData = job
+                .mapper(new FinesMapper())
+                .reducer(new FinesReducer())
+                .submit()
+                .get();
+
+        Map<Integer, Tickets> tickets = getHz().getMap(Util.HAZELCAST_NAMESPACE);
 
     }
 
