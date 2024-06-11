df6aaa5287dd974232795af8466cd87b5cba988c
marco444 <elscili444.code@gmail.com>
2024-06-11 14:02:37 -0300

    added reducer based on a priority queue to keep the top three infractions by city

--- api/src/main/java/ar/edu/itba/pod/query2/InfractionsReducer.java
+++ api/src/main/java/ar/edu/itba/pod/query2/InfractionsReducer.java
@@ -1,4 +1,38 @@
 package ar.edu.itba.pod.query2;
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
 
-public class InfractionsReducer {
+import java.util.*;
+
+public class InfractionsReducer implements ReducerFactory<String, Integer, List<String>> {
+
+    @Override
+    public Reducer<Integer, List<String>> newReducer(String s) {
+        return new Reducer<>() {
+            private transient PriorityQueue<Map.Entry<String, Integer>> topCities;
+
+            @Override
+            public void beginReduce() {
+                topCities = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+            }
+
+            @Override
+            public void reduce(Integer value) {
+                topCities.add(new AbstractMap.SimpleEntry<>(s, value));
+                if (topCities.size() > 3) {
+                    topCities.poll();
+                }
+            }
+
+            @Override
+            public List<String> finalizeReduce() {
+                List<String> result = new ArrayList<>();
+                while (!topCities.isEmpty()) {
+                    result.add(topCities.poll().getKey());
+                }
+                Collections.reverse(result); // To have the highest infraction count first
+                return result;
+            }
+        };
+    }
 }
