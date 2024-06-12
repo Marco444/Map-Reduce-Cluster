1e967013a51e2c1545427e6e604182feb0dd045d
slarroude <slarroude@itba.edu.ar>
2024-06-12 20:38:22 -0300

    bash files to querys examples
    Co-authored-by: martinippo01 <martinippo@gmail.com>
    Co-authored-by: Marco4444 <mscilipoti@itba.edu.ar>
    Co-authored-by: Martín E. Zahnd <mzahnd@itba.edu.ar>

---
+++ scriptTests/query4_NYC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#
+
+BASE_PATH="../client/src/main/java/ar/edu/itba/pod/tpe2/client"
+ADDRESS='localhost:50058'
+CITY='NYC'
+IN_PATH='../example/data'
+OUT_PATH='../output'
+FROM_DATE='01/01/2019'
+TO_DATE='31/12/2020'
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+if ! pushd "${SCRIPT_DIR}" &> /dev/null; then
+        >&2 echo "Script directory not found (??): '$SCRIPT_DIR'"
+        exit 1
+fi
+
+bash query4.sh -Daddresses="$ADDRESS" -DinPath="$IN_PATH" -DoutPath="$OUT_PATH" -Dcity="$CITY" -Dfrom="$FROM_DATE" -Dto="$TO_DATE"
