da58c2a8af9a32fbb0093c0c823207f143ac7b27
Martín E. Zahnd <mzahnd@itba.edu.ar>
2024-06-12 19:30:49 -0300

    Fix server script
    * Script was not taking arguments (starting with `-D`) properly

--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-PATH_TO_CODE_BASE=`pwd`
+# JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"
 
-#JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"	
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+if ! pushd "${SCRIPT_DIR}" &> /dev/null; then
+        >&2 echo "Script directory not found (??): '$SCRIPT_DIR'"
+        exit 1
+fi
 
+PATH_TO_CODE_BASE="$(pwd)"
 MAIN_CLASS="ar.edu.itba.pod.tpe2.server.Server"
 
+java $JAVA_OPTS "$@" -cp 'lib/jars/*' "$MAIN_CLASS"
 
-java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS $*
+# Exit script directory
+popd &> /dev/null || exit 0
+
